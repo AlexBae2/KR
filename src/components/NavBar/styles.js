3bc057bbc5015e4693e88d170be02a74f7478141
Бабичев Анвар <53948721+AlexBae2@users.noreply.github.com>
2023-05-23 20:09:42 +0500

    feat(front): build

--- src/components/NavBar/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const NavBarWrapper = styled.nav`
-  height: 100%;
-  margin-right: 1rem;
-  border-right: 1px solid;
-`;
--- src/components/Points/Literature/styles.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const CustomP = styled.p`
-  margin:10px 0 10px 0;
-`
